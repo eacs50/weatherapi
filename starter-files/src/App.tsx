cddcb9132338576aa22cc5a8f246b84a5463de46
eacs50 <diego@eacs.edu.ar>
2024-07-06 19:46:06 -0300

    chore:start set

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,15 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col texrt-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="text-sm mt-2">
+          Enter velow a place you want to know the Weather of and select an
+          option from the dropdown
+        </p>
+      </section>
     </main>
   )
 }
